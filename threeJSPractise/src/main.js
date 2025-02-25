e7c6a1731a434bebaa0fb15c0cdaf754a992732e
Antek Baginski <antkodziej73@gmail.com>
2025-02-25 20:20:56 +0100

    Dodawanie nowych blokow, dodanie klasy blok

--- threeJSPractise/src/main.js
+++ threeJSPractise/src/main.js
@@ -1,14 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //* SET UP
 let width = 0;
 let height = 0;
 
 const mouse = new THREE.Vector2();
-const scene = new THREE.Scene();
 
+const scene = new THREE.Scene();
 const canvas = document.querySelector('.canvas');
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setClearColor('white', 0);
@@ -16,41 +15,71 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
 camera.position.z = 3;
-// camera.position.x = 3;
-
+camera.position.x = 2;
+camera.position.y = 1;
 scene.add(camera);
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.z = 3;
+pointLight.position.x = 2;
+scene.add(pointLight)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
 
 
 const raycaster = new THREE.Raycaster()
 let intersects = [];
+let currentBlock;
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: 'red', wireframe: true});
+class Cube extends THREE.Mesh{
+  constructor(){
+    super()
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.MeshStandardMaterial({color: '#212529'});
+  }
+  sibblingNextTo = [];
+}
 
-const meshGroup = new THREE.Group();
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-meshGroup.add(cubeMesh)
-scene.add(meshGroup)
-
-const addCube = function(side){
-  const newCubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  newCubeMesh.position.y += meshGroup.children.length
-  meshGroup.add(newCubeMesh);
-  
+Cube.prototype.onClick = function(){
+  this.material.color.setHex(0xc92a2a);
 }
 
 
 
-//! events
+
+
+const meshGroup = new THREE.Group();
+
+const addCube = function(side){
+  const cube = new Cube()
+  meshGroup.add(cube)
+  switch(side){
+    case 'top':
+      cube.position.set(currentBlock.position.x, currentBlock.position.y + 1, currentBlock.position.z)
+      break;
+    case 'right':
+      cube.position.set(currentBlock.position.x + 1, currentBlock.position.y, currentBlock.position.z)
+      break;
+    case 'left':
+      cube.position.set(currentBlock.position.x - 1, currentBlock.position.y, currentBlock.position.z)
+      break;
+  }
+}
+addCube()
+scene.add(meshGroup)
+
+//* EVENTS
 const resize = function(){
   width = window.innerWidth;
   height = window.innerHeight;
   camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    console.log('resize!');
-
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  console.log('resize!');
 };
 resize();
 window.addEventListener('resize', resize);
@@ -64,23 +93,25 @@ document.addEventListener('pointermove', function(e){
 });
 
 window.addEventListener('click', function(e){
-  intersects.forEach(function(el){
-    el.object.material.color.setHex(0x000000)
-  });
+  if(intersects.length > 0){
+  meshGroup.children.forEach(function(e){
+    e.material.color.setHex(0x212529)
+  })
+    intersects[0].object.onClick();
+    currentBlock = intersects[0].object;
+  }
 });
 
-const sunLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(sunLight)
 
-
-
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true;
-// controls.autoRotate = true;
-
-
-
-
+const addBlockBtns = document.querySelectorAll('.block');
+addBlockBtns.forEach(function(el, idx){
+  if(!el.classList.contains('clickedBlock')){
+    el.addEventListener('click', function(){
+      const side = this.dataset.side;
+      addCube(side);
+    });
+  }
+});
 
 const rednerLoop = function(){
   controls.update();
@@ -91,59 +122,3 @@ const rednerLoop = function(){
 rednerLoop();
 
 
-//changing the colors
-
-const btnContainer = document.querySelector('.btnContainer');
-btnContainer.addEventListener('click', function(e){
-  if(e.target.classList.contains('changeColorBtn')){
-    const color = e.target.dataset.color;
-    document.querySelectorAll('.changeColorBtn').forEach(e => e.classList.remove('clicked'));
-    e.target.classList.add('clicked')
-    cubeMesh.material.color.setHex(color);
-  }
-});
-
-//changing the size
-const inputElArr = document.querySelectorAll('.changeSizeInput input');
-
-inputElArr.forEach(function(e, idx){
-  e.addEventListener('input', function(){
-    const value = this.value / 10;
-    switch(idx){
-      case 0:
-        console.log("X value: ", value);
-        cubeMesh.scale.x = value;
-      case 1:
-        console.log("Y value: ", value);
-        cubeMesh.scale.y = value;
-      case 2:
-        console.log("Z value: ", value);
-        cubeMesh.scale.z = value;
-    }
-  });
-});
-
-
-const switchEl = document.querySelector('.switch');
-const ball = document.querySelector('.ball');
-let checked = true;
-
-
-switchEl.addEventListener('click', function(){
-  if(checked){
-    ball.style.animation = 'moveLeft 0.3s forwards ease-in-out';
-    controls.autoRotate = false;
-    switchEl.style.backgroundColor = '#efefef';
-    ball.style.backgroundColor = '#343a40'
-  }else{
-    ball.style.animation = 'moveRight 0.3s forwards ease-in-out';
-    switchEl.style.backgroundColor = '#37b24d';
-    ball.style.backgroundColor = '#efefef'
-    controls.autoRotate = true;
-  }
-  checked = !checked;
-});
-
-const settingsBtn = document.querySelector('.openSettingsBtn');
-const settingsTab = document.querySelector('aside');
-settingsBtn.addEventListener('click', () => settingsTab.classList.toggle('none'));
