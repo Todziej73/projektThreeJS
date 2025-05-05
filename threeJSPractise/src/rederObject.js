ba3274f267b8bba8e64868efc73447037e71b22a
RybsonBybson <miwlod08+1@gmail.com>
2025-05-05 21:05:58 +0200

    Loading Bar stops - Should Fix

--- threeJSPractise/src/rederObject.js
+++ threeJSPractise/src/rederObject.js
@@ -40,17 +40,16 @@ const loadText = function(text, material = new THREE.MeshStandardMaterial({ colo
 };
 
 
-window.addEventListener('load', async () => {
-    mustManager.onLoad = async function () {
-        console.log('Ready to work (MUSTLOADER)!');
-        await addCube(-1);
-        overlay.classList.add('hidden');
-        fill.classList.add('hidden');
-        text.classList.add('hidden');
-        percentage.classList.add('hidden');
-    };
-});
-
+mustManager.onLoad = async function () {
+    console.log('Ready to work (MUSTLOADER)!');
+    await addCube(-1).catch(err => {
+        location.reload();
+    });
+    overlay.classList.add('hidden');
+    fill.classList.add('hidden');
+    text.classList.add('hidden');
+    percentage.classList.add('hidden');
+};
 
 
 
